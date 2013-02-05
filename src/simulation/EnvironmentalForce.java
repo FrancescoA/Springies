2894d43ec46910bce197469aa126884aefb2618d
Jack Matteucci <Jackmatt16@gmail.com>
2013-02-04 23:35:13 -0500

    Modified Environmental Force with comments

--- src/simulation/EnvironmentalForce.java
+++ src/simulation/EnvironmentalForce.java
@@ -2,10 +2,19 @@ package simulation;
 
 import java.util.List;
 
+/**
+ * XXX.
+ * 
+ * @author Jack Matteucci
+ */
+
 public abstract class EnvironmentalForce {
 
 	public EnvironmentalForce() {
 
 	}
+	/**
+	 * Method to be extended by all EnvironmentalForces
+	 */
 	public abstract void Apply(List<Mass> Masses);
 }
