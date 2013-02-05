96ae0533bd726e150fd37c539f8aa20dfc30235a
Jack Matteucci <Jackmatt16@gmail.com>
2013-02-04 23:42:48 -0500

    Modified Gravity to only contain essential methods and comments

--- src/simulation/Gravity.java
+++ src/simulation/Gravity.java
@@ -3,28 +3,37 @@ package simulation;
 import java.util.List;
 import java.awt.Dimension;
 import util.Vector;
+/**
+ * XXX.
+ * 
+ * @author Jack Matteucci
+ */
 
 public class Gravity extends EnvironmentalForce{
     private Vector myAcceleration;
-    
+ 
+    /**
+     * Create a gravity object with a direction and magnitude 
+     */
 	public Gravity(Double direction, Double magnitude) {
 		super();
 		myAcceleration = new Vector(direction, magnitude);
 	}
 	
 	@Override
+	/**
+	 * Cycles through a list of Masses, applying the constructed gravity to each 
+	 * of these forces
+	 */
 	public void Apply(List<Mass> Masses){
 		for(Mass m : Masses){
 			m.applyForce(myAcceleration);
 		}
 	}
+	/**
+	 * returns the Gravity object's acceleration constant
+	 */
 	public Vector getAcceleration(){
 		return myAcceleration;
-	}
-	public void scaleMagnitude(double change){
-		myAcceleration.scale(change);
-	}
-	public void setAngle(double angle){
-		myAcceleration = new Vector(angle, myAcceleration.getMagnitude());
 	}
 }
